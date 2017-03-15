ab5ca3d3800849b4a65d80bbfc977c24ce11e9b4
Mike Young <mcy001@gmail.com>
2017-03-15 01:13:53 -0500

    finished exposing all stats

--- functions.php
+++ functions.php
@@ -8,8 +8,11 @@
  /* BEGIN MIKE YOUNG FUNCTIONS */
  function printFormattedAttribute($label,$value,$modifiers = '') {
 	  echo "<div><span class='field-label'>$label:</span> <span class='field-value'>$value";
-	  if ($modifiers != "") {
-	  	echo  " (Modifiers: $modifiers)";
+	  if ($label == 'Strength' && get_field('exceptional_strength') > 0) {
+		  echo  "/".get_field('exceptional_strength');
+	  }
+	  if ($modifiers != "" && strtolower($modifiers) != "nil") {
+	  	echo  " (Mod: $modifiers)";
 	  }
 	  echo "</span></div>";
  }
--- single.php
+++ single.php
@@ -12,9 +12,58 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
 
+
+			<img class="character-portrait" src="<?= wp_get_attachment_url( get_post_thumbnail_id(), 'large') ?>" />
+			<div class="character-overview">
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					<?php printFormattedAttribute('Player Name',get_field('player_name')) ?>
+					<?php printFormattedAttribute('Alignment',get_field('alignment')) ?>
+					<?php printFormattedAttribute('Race',get_field('race')) ?>
+					<?php printFormattedAttribute('Class',get_field('class')) ?>
+					<?php printFormattedAttribute('Age',get_field('age')) ?>
+					<?php printFormattedAttribute('Sex',get_field('sex')) ?>
+					<?php printFormattedAttribute('Height',get_field('height')) ?>
+					<?php printFormattedAttribute('Weight',get_field('weight')) ?>
+					<?php printFormattedAttribute('Base THAC0',get_field('base_thac0')) ?>
+					<?php printFormattedAttribute('Move',get_field('movement'),get_field('movement_modifiers')) ?>
+					<?php printFormattedAttribute('Max. Hit Points',get_field('maximum_hit_points')) ?>
+					<?php printFormattedAttribute('Exp. Points',get_field('experience_points')) ?>
+				</header><!-- .entry-header -->
+			</div>
+			<br class="clear" />
+			<div class="stat-box">
+				<h3>ABILITY SCORES</h3>
+				<ul class="stat-list">
+					<li><?php printFormattedAttribute('Strength',get_field('strength')) ?></li>
+					<li><?php printFormattedAttribute('Dexterity',get_field('dexterity')) ?></li>
+					<li><?php printFormattedAttribute('Constitution',get_field('constitution')) ?></li>
+					<li><?php printFormattedAttribute('Intelligence',get_field('intelligence')) ?></li>
+					<li><?php printFormattedAttribute('Wisdom',get_field('wisdom')) ?></li>
+					<li><?php printFormattedAttribute('Charisma',get_field('charisma')) ?></li>
+				</ul>
+			</div>
+			<div class="stat-box">
+				<h3>SAVING THROWS</h3>
+				<ul class="stat-list">
+					<li><?php printFormattedAttribute('Paralyzation, Poison, or Death Magic',get_field('save_vs_paralyzation_poison_or_death_magic'),get_field('save_vs_paralyzation_poison_or_death_magic_modifiers')) ?></li>
+					<li><?php printFormattedAttribute('Rod, Staff, or Wand',get_field('save_vs_rod_staff_or_wand'),get_field('save_vs_rod_staff_or_wand_modifiers')) ?></li>
+					<li><?php printFormattedAttribute('Petrification or Polymorph',get_field('save_vs_petrification_or_polymorph'),get_field('save_vs_petrification_or_polymorph_modifiers')) ?></li>
+					<li><?php printFormattedAttribute('Breath Weapon',get_field('save_vs_breath_weapon'),get_field('save_vs_breath_weapon_modifiers')) ?></li>
+					<li><?php printFormattedAttribute('Spell',get_field('save_vs_spell'),get_field('save_vs_spell_modifiers')) ?></li>
+				</ul>
+			</div>
+			<div class="stat-box">
+				<h3>ARMOR CLASS</h3>
+				<ul class="stat-list">
+					<li><?php printFormattedAttribute('Full',get_field('armor_class_full'),get_field('armor_class_modifiers')) ?></li>
+					<li><?php printFormattedAttribute('Shieldless',get_field('armor_class_shieldless')) ?></li>
+					<li><?php printFormattedAttribute('Surprised / Immobile',get_field('armor_class_surprised__immobile')) ?></li>
+				</ul>
+			</div>
+			<br class="clear" />
 
 			<?php
 				// check if the repeater field has rows of data
@@ -24,7 +73,7 @@ get_header(); ?>
 					while ( have_rows('weapon_profiles') ) : the_row();
 			?>
 						<div class="weapon-details">
-							<div class="weapon-name"><?= get_sub_field('weapon') ?></div>
+							<h3><?= get_sub_field('weapon') ?></h3>
 							<?php printFormattedAttribute('No. of Attacks',get_sub_field('number_of_attacks'),get_sub_field('number_of_attacks_modifiers')) ?>
 							<?php printFormattedAttribute('Rate of Fire',get_sub_field('rate_of_fire')) ?>
 							<?php printFormattedAttribute('Attack Adjustment',get_sub_field('attack_adjustment'),get_sub_field('attack_adjustment_modifiers')) ?>
@@ -54,7 +103,7 @@ get_header(); ?>
 									<td>AC Hit</td>
 								</tr>
 							</table>
-							<table class="thac0-table thac0-table-left">
+							<table class="thac0-table thac0-table-left horz-table">
 								<tr>
 									<th>1</th>
 									<th>2</th>
@@ -70,7 +119,7 @@ get_header(); ?>
 									<td><?= $weapon_thac0 - 5 ?></td>
 								</tr>
 							</table>
-							<table class="thac0-table thac0-table-right">
+							<table class="thac0-table thac0-table-right horz-table">
 								<tr>
 									<th>6</th>
 									<th>7</th>
@@ -86,7 +135,7 @@ get_header(); ?>
 									<td><?= $weapon_thac0 - 10 ?></td>
 								</tr>
 							</table>
-							<table class="thac0-table thac0-table-left">
+							<table class="thac0-table thac0-table-left horz-table">
 								<tr>
 									<th>11</th>
 									<th>12</th>
@@ -102,7 +151,7 @@ get_header(); ?>
 									<td><?= $weapon_thac0 - 15 ?></td>
 								</tr>
 							</table>
-							<table class="thac0-table thac0-table-right">
+							<table class="thac0-table thac0-table-right horz-table">
 								<tr>
 									<th>16</th>
 									<th>17</th>
@@ -134,7 +183,7 @@ get_header(); ?>
 				endif;
 			?>
 
-
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
 		<?php endwhile; // End of the loop. ?>
 
